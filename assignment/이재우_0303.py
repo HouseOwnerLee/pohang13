a9b062338a685a1a791c90490d83ed6dc152c3a2
HouseOwnerLee <an3ppna@naver.com>
2026-03-03 17:51:10 +0900

    260303

---
+++ assignment/이재우_0303.py
@@ -0,0 +1,54 @@
+﻿cnt = 0
+
+def input_nums():
+    num1 = int(input("첫번째 숫자 입력: "))
+    num2 = int(input("두번째 숫자 입력: "))
+    print()
+
+    return num1, num2
+
+def min_max_num(num1, num2):
+    if num1 > num2:
+        min_num = num2
+        max_num = num1
+
+    else:
+        min_num = num1
+        max_num = num2
+
+    return min_num, max_num
+
+def print_primes(min_num, max_num):
+    global cnt
+    for num in range(num1, num2 + 1):
+        if num < 2:
+            continue
+        elif num == 2:
+            print("%3d" % num, end=' ')
+            cnt += 1
+            if cnt % 10 == 0:
+                print()
+        else:
+            if num % 2 == 0:
+                continue
+            for i in range(3, int(num ** 0.5) + 1, 2):
+                if num % i == 0:
+                    break
+            else:
+                print("%3d" % num, end=' ')
+                cnt += 1
+                if cnt % 10 == 0:
+                    print()
+    return
+
+def print_total():
+    if cnt % 10 != 0:
+        print()
+    print("소수의 갯수 = %d" % cnt)
+    return
+
+if __name__ == "__main__":
+    num1, num2 = input_nums()
+    min_num, max_num = min_max_num(num1, num2)
+    print_primes(min_num, max_num)
+    print_total()
--- python/function_practice/recursive_call_function.py
+++ python/function_practice/recursive_call_function.py
@@ -1,10 +1,11 @@
 ﻿def factorial(n):
-    # if n == 0:
-    #     return 1
-    # return n * factorial(n-1)
-    result = 1
-    for i in range(n, 0, -1):
-        result *= i
-    return result
+    if n == 0:
+        return 1
+    return n * factorial(n-1)
+    # for문으로 구현 가능
+    # result = 1
+    # for i in range(n, 0, -1):
+    #     result *= i
+    # return result
 
 print(factorial(5))
