1b0bb321afd526b46ca3f795d548dc4efeb97293
HouseOwnerLee <an3ppna@naver.com>
2026-03-03 15:47:42 +0900

    260303

---
+++ python/function_practice/recursive_call_function.py
@@ -0,0 +1,10 @@
+﻿def factorial(n):
+    # if n == 0:
+    #     return 1
+    # return n * factorial(n-1)
+    result = 1
+    for i in range(n, 0, -1):
+        result *= i
+    return result
+
+print(factorial(5))
