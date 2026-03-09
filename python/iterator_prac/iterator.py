d9dc098e3717f73f6226127075b52ada112ad609
HouseOwnerLee <an3ppna@naver.com>
2026-03-09 10:06:48 +0900

    260309

---
+++ python/iterator_prac/iterator.py
@@ -0,0 +1,21 @@
+﻿class Counter:
+    def __init__(self, stop):
+        self.current = 0 # 현재 숫자
+        self.stop = stop # 반복을 끝낼 숫자
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        # 현재 숫자가 반복을 끝낼 숫자보다 작을 때
+        if self.current < self.stop:
+            r = self.current # 현재 숫자를 변수 r에 저장
+            self.current += 1 # 현재 숫자를 1 증가시킴
+            return r # 숫자 반환
+        else:
+            # 현재 숫자가 반복을 끝낼 숫자보다 크거나 같을 때 예외
+            raise StopIteration
+
+if __name__ == '__main__':
+    for i in Counter(3):
+        print(i, end=' ')
---
+++ python/iterator_prac/iterator_getitem.py
@@ -0,0 +1,16 @@
+﻿class Counter:
+    def __init__(self, stop):
+        self.stop = stop
+
+    # __getitem__만 구현해도 이터레이터가 됨, __iter__, __next__ 생략 가능
+    def __getitem__(self, index):
+        if index < self.stop:
+            return index
+        else:
+            raise IndexError
+
+if __name__ == '__main__':
+    print(Counter(3)[0], Counter(3)[1], Counter(3)[2])
+
+    for i in Counter(3):
+        print(i, end=' ')
